a27d3bf7a481ebc4c1d3acca09c90c6242e7f467
ehdtjs0612 <inko51366@naver.com>
2024-01-26 16:05:27 +0900

    feat: import created module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AccountModule } from './api/account/account.module';
+import { AuthModule } from './api/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AccountModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
