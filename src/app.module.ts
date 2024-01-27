b728f63c1135dac83ff6982927fa6e2663837759
ehdtjs0612 <inko51366@naver.com>
2024-01-27 23:52:53 +0900

    feat: add prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AccountModule } from './api/account/account.module';
 import { AuthModule } from './api/auth/auth.module';
+import { PrismaService } from './common/prisma/prisma.service';
 
 @Module({
   imports: [
