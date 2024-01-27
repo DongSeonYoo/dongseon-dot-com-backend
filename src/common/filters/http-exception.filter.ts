c17a4918dffbf3863f394679d7343c9efd6d2e47
ehdtjs0612 <inko51366@naver.com>
2024-01-28 03:12:34 +0900

    feat: add global exception filter

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,26 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { instanceToPlain } from 'class-transformer';
+import { Response } from 'express';
+import { ResponseEntity } from '../dto/common-response.dto';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(e: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const res: Response = ctx.getResponse();
+
+    const statusCode = e.getStatus();
+    let errorMessage = e.getResponse()['message'];
+
+    res
+      .status(statusCode)
+      .json(
+        instanceToPlain(ResponseEntity.ERROR_WITH(statusCode, errorMessage)),
+      );
+  }
+}
