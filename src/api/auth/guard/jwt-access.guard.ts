68cb4e06f3804ce93114976026300b406862683c
ehdtjs0612 <inko51366@naver.com>
2024-01-29 17:43:24 +0900

    feat: when the jwt manipulation, break accessToken

--- src/api/auth/guard/jwt-access.guard.ts
+++ src/api/auth/guard/jwt-access.guard.ts
@@ -4,11 +4,15 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { error } from 'console';
+import { Response } from 'express';
 
 @Injectable()
 export class JwtAccessGuard extends AuthGuard('jwt') {
   handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
     if (!user) {
+      const response: Response = context.switchToHttp().getResponse();
+      response.clearCookie('accessToken');
       throw new UnauthorizedException('로그인 후 이용가능합니다');
     }
 
