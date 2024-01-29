36a5f7bf52066fcd84e951bc7b5ca3c2cfbb7e93
ehdtjs0612 <inko51366@naver.com>
2024-01-29 17:51:09 +0900

    refactor: refactor Jwt module to use async factory

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -2,18 +2,27 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { MailModule } from 'src/common/mail/mail.module';
+import { ConfigService } from '@nestjs/config';
+import { RedisModule } from 'src/common/redis/redis.module';
+import { PrismaService } from 'src/common/prisma/prisma.service';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: process.env.JWT_SECRET_KEY,
-      signOptions: {
-        expiresIn: process.env.JWT_EXPIRED_TIME,
-      },
+    RedisModule,
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET_KEY'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRED_TIME'),
+        },
+      }),
+      inject: [ConfigService],
     }),
+    MailModule,
   ],
   exports: [AuthService],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService],
 })
 export class AuthModule {}
