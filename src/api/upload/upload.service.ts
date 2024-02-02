d58cf83be2f0651fbf95161c69de8351838bd0ad
ehdtjs0612 <inko51366@naver.com>
2024-02-02 22:29:19 +0900

    feat: add upload image file to s3 api

--- src/api/upload/upload.service.ts
+++ src/api/upload/upload.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UploadService {}
+export class UploadService {
+  extractImageLocation(files: Express.MulterS3.File[]): string[] {
+    return files.map((file) => file.location);
+  }
+}
