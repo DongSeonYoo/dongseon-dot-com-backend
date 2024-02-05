ea0e73c0cca71e931b4866297f0c58e9b2507956
ehdtjs0612 <inko51366@naver.com>
2024-02-05 11:49:45 +0900

    refactor: remove upload api endpoint

--- src/api/upload/upload.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class UploadService {
-  extractImageLocation(files: Express.MulterS3.File[]): string[] {
-    return files.map((file) => file.location);
-  }
-}
