08d911a14b0ffbbd38a3ed5e3e9c92e0c8716715
ehdtjs0612 <inko51366@naver.com>
2024-02-06 15:28:05 +0900

    feat: add common response dto

--- src/common/dto/common-response.dto.ts
+++ src/common/dto/common-response.dto.ts
@@ -1,41 +1,20 @@
-import { HttpStatus } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsJSON, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
-export class ResponseEntity<T> {
-  statusCode: HttpStatus;
+export class CommonResponseDto {
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty({
+    default: 200,
+  })
+  statusCode: number;
 
+  @IsString()
+  @IsJSON()
+  @ApiProperty()
   message: string;
 
-  data: T;
-
-  private constructor(status: HttpStatus, message: string, data: T) {
-    this.statusCode = status;
-    this.message = message;
-    this.data = data;
-  }
-
-  static SUCCESS(
-    message: string = '',
-    httpStatus: HttpStatus = HttpStatus.OK,
-  ): ResponseEntity<string> {
-    return new ResponseEntity<string>(httpStatus, message, '');
-  }
-
-  static SUCCESS_WITH<T>(data: T, message: string = ''): ResponseEntity<T> {
-    return new ResponseEntity<T>(HttpStatus.OK, message, data);
-  }
-
-  static ERROR(): ResponseEntity<string> {
-    return new ResponseEntity<string>(
-      HttpStatus.INTERNAL_SERVER_ERROR,
-      '서버에서 에러가 발생하였습니다',
-      '',
-    );
-  }
-
-  static ERROR_WITH(
-    code: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
-    message: string,
-  ): ResponseEntity<string> {
-    return new ResponseEntity<string>(code, message, '');
-  }
+  @IsJSON()
+  @ApiProperty()
+  data: Object;
 }
