418cd84c1910d7e558be7886c101f79480e58c02
ehdtjs0612 <inko51366@naver.com>
2024-02-08 14:42:29 +0900

    refactor: refactor validation message formmat in global exception filter

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -1,31 +1,29 @@
 import {
   ArgumentsHost,
+  BadRequestException,
   Catch,
   ExceptionFilter,
   HttpException,
 } from '@nestjs/common';
 import { instanceToPlain } from 'class-transformer';
 import { Response } from 'express';
-import { ResponseEntity } from '../dto/common-response.dto';
+import { ResponseEntity } from '../common-response';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(e: HttpException, host: ArgumentsHost) {
+    console.log(e);
     const ctx = host.switchToHttp();
     const res: Response = ctx.getResponse();
 
     const statusCode = e.getStatus();
     let errorMessage = (e.getResponse() as Error).message;
 
-    console.log(e);
-    if (Array.isArray(errorMessage)) {
-      errorMessage = errorMessage[0];
-    }
-
+    // if (Array.isArray(errorMessage)) {
+    //   errorMessage = errorMessage[0];
+    // }
     res
       .status(statusCode)
-      .json(
-        instanceToPlain(ResponseEntity.ERROR_WITH(statusCode, errorMessage)),
-      );
+      .json(ResponseEntity.ERROR_WITH(statusCode, errorMessage));
   }
 }
