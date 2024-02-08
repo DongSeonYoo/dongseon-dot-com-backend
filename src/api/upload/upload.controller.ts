3c1373ee8860125b06c3c0155974fd5eca55412b
ehdtjs0612 <inko51366@naver.com>
2024-02-08 18:30:14 +0900

    refactor: validate file upload request

--- src/api/upload/upload.controller.ts
+++ src/api/upload/upload.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Controller,
   Post,
   UploadedFile,
@@ -8,16 +9,23 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { JwtAccessGuard } from '../auth/guard/jwt-access.guard';
-import { ConfigService } from '@nestjs/config';
 import { ResponseEntity } from 'src/common/common-response';
+import { ApiTags } from '@nestjs/swagger';
+import s3Storage from 'multer-s3';
 
+@ApiTags('upload')
 @Controller('upload')
 export class UploadController {
-  constructor(configService: ConfigService) {}
+  constructor() {}
+
   @Post('profile-image')
   @UseGuards(JwtAccessGuard)
   @UseInterceptors(FileInterceptor('profileImage'))
-  async uploadProfileImg(@UploadedFile() file: Express.MulterS3.File) {
+  async uploadProfileImg(@UploadedFile() file: Express.MulterS3.File | null) {
+    if (!file) {
+      throw new BadRequestException('파일이 존재하지 않습니다');
+    }
+
     return ResponseEntity.SUCCESS_WITH({
       location: file.location,
     });
@@ -25,9 +33,20 @@ export class UploadController {
 
   @Post('post-images')
   @UseGuards(JwtAccessGuard)
-  @UseInterceptors(FilesInterceptor('postImages', 10))
-  async uploadPostImages(@UploadedFiles() files: Express.MulterS3.File[]) {
-    const location = files.map(({ location }) => location);
+  @UseInterceptors(
+    FilesInterceptor('postImages', 10, {
+      limits: {
+        files: 10,
+      },
+    }),
+  )
+  async uploadPostImages(
+    @UploadedFiles() files: Express.MulterS3.File[] | null,
+  ) {
+    if (!files || files.length === 0) {
+      throw new BadRequestException('파일이 존재하지 않습니다');
+    }
+    const location = files.map((file) => file.location);
 
     return ResponseEntity.SUCCESS_WITH({
       location,
